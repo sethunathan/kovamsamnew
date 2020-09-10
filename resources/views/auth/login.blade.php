7ef56170a424a9f470b2080423d765f104b6081e
satheesh <satheesh@dreamstudio.in>
2020-09-10 17:40:18 +0530

    fix login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,10 +15,10 @@
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
+           
             <div>
-                <x-jet-label value="Mobile No" />
-                <x-jet-input class="block mt-1 w-full" type="text" name="contactno" :value="old('contactno')" 
-                       required autofocus />
+                <x-jet-label value="Email" />
+                <x-jet-input class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="mt-4">
